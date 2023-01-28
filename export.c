f7e7d0d24acb16558f2b3677a7703c3a03bb4986
Joao Pedro Egger <jpedroegger@gmail.com>
2023-01-28 14:10:17 +0000

    export and new env

---
+++ export.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jegger-s <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/28 13:23:25 by jegger-s          #+#    #+#             */
+/*   Updated: 2023/01/28 13:23:28 by jegger-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "minishell.h"
+
+void	ft_export(char *command, char **new_env)
+{
+	int	i;
+	
+	i = 0;
+	if (!ft_strncmp(command, "export.", 7))
+	{
+		while (new_env[i])
+		{
+			printf("%s\n", new_env[i]);
+			i++;
+		}
+		printf("%d\n", i);
+	}
+	else if (!ft_strncmp(command, "export ", 7))
+	{
+		while (new_env[i])
+			i++;
+		if (ft_strchr(command, '='))
+			new_env[i] = ft_strdup(&command[7]);
+		else
+		{
+			new_env[i] = ft_strjoin(&command[7], "=''");
+		}
+		printf("Included: %s\n", new_env[i]);
+	}
+}
---
+++ set_new_env.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_new_env.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jegger-s <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/28 14:06:05 by jegger-s          #+#    #+#             */
+/*   Updated: 2023/01/28 14:06:07 by jegger-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "minishell.h"
+
+static size_t	get_sizeof_envp(char **envp)
+{
+	size_t	total_length;
+	int		i;
+
+	i = -1;
+	total_length = 0;
+	while (envp[++i])
+		total_length += ft_strlen(envp[i]);
+	return (total_length + i);
+}
+
+char	**set_new_env(char **envp)
+{
+	size_t	total_length;
+	char	**new_env;
+	int		i;
+
+	total_length = get_sizeof_envp(envp);
+	new_env = malloc(sizeof(char *) * total_length + 1);
+	i = -1;
+	while (envp[++i])
+		new_env[i] = ft_strdup(envp[i]);
+	return (new_env);
+}
