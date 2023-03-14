a5a5d4d16beb4d59fc403455c557493035b5d8ce
Francisco Vieira <fvieira@c1r15s14.42porto.com>
2023-03-14 20:36:49 +0000

    mult pipes being handled

--- pipe.c
+++ pipe.c
@@ -63,9 +63,55 @@ void	pipes(t_prompt *everything, int c)
 	everything->exit_stat = status >> 8;
 }
 
-/*void	mult_pipes(t_prompt *every, int c, int mult)
+void	alt_child(t_prompt *every, int pfd[2], int c)
+{
+	char	**seperated;
+
+	seperated = initialize(every, c);
+	dup(pfd[1]);
+	close(pfd[0]);
+	close(pfd[1]);
+	idk(seperated[0], seperated, every, c);
+	freesplit(seperated);
+}
+
+void	mult_pipes(t_prompt *every, int c, int mult)
 {
 	pid_t pid;
 	int	status;
-	int pfd[2];
-}*/
+	int pfd[mult * 2];
+	int	i;
+
+	i = 0;
+	while (i < mult)
+	{
+		if (pipe(&pfd[i * 2]) == -1)
+		{
+			printf("Error");
+			exit(1);
+		}
+		i++;
+	}
+	pid = fork();
+	if (pid == 0)
+		child(everything, &pfd[2], c);
+	waitpid(pid, &status, 0);
+	everything->exit_stat = status >> 8;
+	i = 1;
+	while (every->sep[c + i] && every->sep[c + i][0] == '|')
+	{
+		pid = fork();
+		if (pid == 0)
+			alt_child(everything, &pfd[i * 2], c + i);
+		waitpid(pid, &status, 0);
+		everything->exit_stat = status >> 8;
+		i++;
+	}
+	pid = fork();
+	if (pid == 0)
+		parent(everything, &pfd[i * 2], c + i);
+	waitpid(pid, &status, 0);
+	everything->exit_stat = status >> 8;
+	while (i > 0)
+		close(pfd[i--]);
+}
