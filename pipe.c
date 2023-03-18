44034b82e008fd91581a3fb37c5fe8c91f660d55
Francisco Vieira <fvieira@c1r10s13.42porto.com>
2023-03-18 21:36:10 +0000

    almost done

--- pipe.c
+++ pipe.c
@@ -12,51 +12,120 @@
 
 #include "minishell.h"
 
-void	child(t_prompt *every, int pfd[2], int c)
+void	closefd(int *pfd, int mult)
+{
+	int	i;
+
+	i = 0;
+	while (i < mult * 2 - 2)
+	{
+		//printf("pfd%d\n", pfd[i]);
+		close(pfd[i]);
+		i++;
+	}
+}
+void	child(t_prompt *every, int *pfd, int mult, int c)
 {
 	char	**seperated;
 
 	printf("child %d %d \n",pfd[0], pfd[1] );
 	printf("cmd %s\n",every->cmd[c]);
-	seperated = initialize(every, c);	
+	seperated = initialize(every, c);
 	close(every->fd);
-	dup(pfd[1]);
-	close(pfd[0]);
-	close(pfd[1]);
+	if (dup2(pfd[1], STDOUT_FILENO) < 0)
+		printf("error\n");
+	closefd(pfd, mult);
 	idk(seperated[0], seperated, every, c);
 	freesplit(seperated);
 }
 
-void	alt_child(t_prompt *every, int pfd[2], int c)
+void	alt_child(t_prompt *every, int *pfd, int i, int mult, int c)
 {
 	char	**seperated;
 
-	printf("alt_child %d %d \n",pfd[0], pfd[1]);
+	printf("alt_child  old %d %d \n", pfd[i - 2], pfd[i - 1]);	
+	printf("alt_child  new %d %d \n", pfd[i], pfd[i + 1]);
 	printf("cmd %s\n",every->cmd[c]);
 	seperated = initialize(every, c);
-	close(STDOUT_FILENO);
-	dup(pfd[1]);
-	close(pfd[0]);
-	close(pfd[1]);
+	dup2(pfd[i + 1], STDOUT_FILENO);
+	if (dup2(pfd[i - 2], STDIN_FILENO) < 0)
+		printf("ERROR\n");
+	closefd(pfd, mult);
 	idk(seperated[0], seperated, every, c);
 	freesplit(seperated);
 }
 
-void	parent(t_prompt *every, int pfd[2], int c)
+void	parent(t_prompt *every, int *pfd, int i, int mult, int c)
 {
 	char	**seperated;
 
-	printf("parent %d %d \n",pfd[0], pfd[1]);
+	printf("parent %d %d \n",pfd[i - 2], pfd[i - 1]);
 	printf("cmd %s\n",every->cmd[c]);
 	seperated = initialize(every, c);
 	close(STDIN_FILENO);
-	dup(pfd[0]);
-	close(pfd[1]);
-	close(pfd[0]);
+	if (dup(pfd[i - 2]) < 0)
+		printf("error\n");
+	closefd(pfd, mult);
 	idk(seperated[0], seperated, every, c);
 	freesplit(seperated);
 }
 
+void	mult_pipes(t_prompt *every, int c, int mult)
+{
+	pid_t pid;
+	int	status;
+	int pfd[(mult - 1) * 2];
+	int	i;
+
+	i = 0;
+	printf("mult %d\n", mult);
+	while (i < mult - 1)
+	{
+		if (pipe(pfd + i * 2) == -1)
+		{
+			printf("Error");
+			exit(1);
+		}
+		i++;
+	}
+	pid = fork();
+	if (pid == 0)
+		child(every, pfd, mult, c);
+	c++;
+	//waitpid(pid, &status, 0);
+	//every->exit_stat = status >> 8;
+	i = 1;
+	while (mult > 1 && every->sep[c] && every->sep[c][0] == '|')
+	{
+		//printf("loop cmd %s\n", every->cmd[c]);
+		pid = fork();
+		if (pid == 0)
+			alt_child(every, pfd, i + 1, mult,  c);
+		i += 2;
+		c++;
+		// waitpid(pid, &status, 0);
+		//every->exit_stat = status >> 8;
+		//printf("ola \n");
+
+	}
+	/*int o;
+	o = 0;
+	while (o < mult)
+	{
+		//printf("pfd%d\n", pfd[o]);
+		waitpid(-1, &status, 0);
+		every->exit_stat = status >> 8;
+		o++;
+	}*/
+	pid = fork();
+	if (pid == 0)
+		parent(every, pfd, i + 1, mult, c);
+	closefd(pfd, mult);
+	waitpid(pid, &status, 0);
+	every->exit_stat = status >> 8;
+
+}
+/*
 void	pipes(t_prompt *everything, int c)
 {
 	pid_t pid;
@@ -80,62 +149,4 @@ void	pipes(t_prompt *everything, int c)
 	close(pfd[1]);
 	waitpid(pid, &status, 0);
 	everything->exit_stat = status >> 8;
-}
-
-void	mult_pipes(t_prompt *every, int c, int mult)
-{
-	pid_t pid;
-	int	status;
-	int pfd[(mult) * 2];
-	int	i;
-
-	i = 0;
-	printf("mult %d\n", mult);
-	while (i < mult)
-	{
-		if (pipe(pfd + i * 2) == -1)
-		{
-			printf("Error");
-			exit(1);
-		}
-		i++;
-	}
-	pid = fork();
-	if (pid == 0)
-		child(every, pfd, c);
-	c++;
-	//waitpid(pid, &status, 0);
-	//every->exit_stat = status >> 8;
-	i = 1;
-	while (mult > 1 && every->sep[c] && every->sep[c][0] == '|')
-	{
-		//printf("loop cmd %s\n", every->cmd[c]);
-		pid = fork();
-		if (pid == 0)
-			alt_child(every, &pfd[i + 1], c);
-		i += 2;
-		c++;
-		waitpid(pid, &status, 0);
-		every->exit_stat = status >> 8;
-		//printf("ola \n");
-
-	}
-	pid = fork();
-	if (pid == 0)
-		parent(every, &pfd[i - 1], c);
-	i = 0;
-	while (i < mult * 2)
-	{
-		//printf("pfd%d\n", pfd[i]);
-		close(pfd[i]);
-		i++;
-	}
-	i = 0;
-	while (i < mult + 3)
-	{
-		//printf("pfd%d\n", pfd[i]);
-		waitpid(pid, &status, 0);
-		every->exit_stat = status >> 8;
-		i++;
-	}
-}
+}*/
